27220906555a3493187f810b32048de73cb780fd
XenoverseUp <xenoverseup01@gmail.com>
2023-03-21 14:10:43 +0300

    Intro animation.

--- src/components/IntroAnimation.jsx
+++ src/components/IntroAnimation.jsx
@@ -5,23 +5,173 @@ import { Fragment, useLayoutEffect } from 'react'
 import Paths from 'components/Paths'
 import { bubbleTimeline, starTimeline } from 'animations/introAnimations'
 import { removeLoader, animateLoader } from 'animations/loaderAnimations'
+import gsap, {
+  Linear,
+  Sine,
+  Power3,
+  Power2,
+  Power1,
+  Power4,
+  Expo,
+  Circ,
+} from 'gsap'
+import MotionPathPlugin from 'gsap/MotionPathPlugin'
+
+gsap.registerPlugin(MotionPathPlugin)
 
 const IntroAnimation = () => {
   useLayoutEffect(() => {
     ;(async () => {
-      animateLoader()
-      await wait(2000)
-      removeLoader()
-      for (let i = 0; i < 7; i++) {
-        starTimeline(`#star-${i}`, `#star-path-${i}`).play()
-        bubbleTimeline(`#bubble-${i}`, `#bubble-path-${i}`).play()
-      }
+      // animateLoader()
+      // await wait(2000)
+      // removeLoader()
+      // for (let i = 0; i < 7; i++) {
+      //   starTimeline(`#star-${i}`, `#star-path-${i}`).play()
+      //   bubbleTimeline(`#bubble-${i}`, `#bubble-path-${i}`).play()
+      // }
     })()
+
+    gsap.set('#star-0', {
+      xPercent: -50,
+      yPercent: -50,
+      '--size': 650, //'clamp(200px, 40%, 400px);',
+
+      opacity: 1,
+    })
+
+    gsap
+      .timeline()
+      .to('#star-0', {
+        duration: 3.5,
+        motionPath: {
+          path: '#star-path-0',
+          align: '#star-path-0',
+          autoRotate: true,
+          alignOrigin: [0.5, 0.5],
+        },
+        scale: 0.7,
+        immediateRender: true,
+        ease: Power4.easeInOut,
+      })
+      .set('#star-1', {
+        display: 'none',
+      })
+
+    gsap.set('#star-1', {
+      xPercent: -50,
+      yPercent: -50,
+      '--size': 650,
+
+      opacity: 1,
+    })
+
+    gsap
+      .timeline()
+      .to('#star-1', {
+        duration: 5,
+        motionPath: {
+          path: '#star-path-1',
+          align: '#star-path-1',
+          autoRotate: true,
+          alignOrigin: [0.5, 0.5],
+        },
+        keyframes: {
+          scale: [1, 0.8, 0.5, 0.2, 0.15],
+        },
+        immediateRender: true,
+        ease: Power4.easeInOut,
+      })
+      .set('#star-1', {
+        display: 'none',
+      })
+
+    gsap.set('#star-2', {
+      xPercent: -50,
+      yPercent: -50,
+      '--size': 650,
+
+      opacity: 1,
+    })
+
+    gsap
+      .timeline()
+      .to('#star-2', {
+        duration: 5,
+        motionPath: {
+          path: '#star-path-2',
+          align: '#star-path-2',
+          autoRotate: true,
+          alignOrigin: [0.5, 0.5],
+        },
+        scale: 0.15,
+        immediateRender: true,
+        ease: Power4.easeInOut,
+      })
+      .set('#star-2', {
+        display: 'none',
+      })
+
+    gsap.set('#star-3', {
+      xPercent: -50,
+      yPercent: -50,
+      '--size': 650,
+
+      opacity: 1,
+    })
+
+    gsap
+      .timeline()
+      .to('#star-3', {
+        duration: 4.5,
+        motionPath: {
+          path: '#star-path-3',
+          align: '#star-path-3',
+          autoRotate: true,
+          alignOrigin: [0.5, 0.5],
+        },
+        keyframes: {
+          scale: [1, 1, 0.8, 0.5, 0.3, 0.1],
+        },
+        immediateRender: true,
+        ease: Power2.easeInOut,
+      })
+      .set('#star-3', {
+        display: 'none',
+      })
+
+    gsap.set('#star-4', {
+      xPercent: -50,
+      yPercent: -50,
+      '--size': 650,
+
+      opacity: 1,
+    })
+
+    gsap
+      .timeline()
+      .to('#star-4', {
+        duration: 5,
+        motionPath: {
+          path: '#star-path-4',
+          align: '#star-path-4',
+          autoRotate: true,
+          alignOrigin: [0.5, 0.5],
+        },
+
+        keyframes: {
+          scale: [1, 1, 0.8, 0.5, 0.3, 0.1],
+        },
+        immediateRender: true,
+        ease: Power2.easeInOut,
+      })
+      .set('#star-4', {
+        display: 'none',
+      })
   }, [])
 
   return (
     <div>
-      {new Array(7).fill(null).map((_, i) => (
+      {new Array(5).fill(null).map((_, i) => (
         <Fragment key={`star-${i}`}>
           <svg
             className={styles.star}
