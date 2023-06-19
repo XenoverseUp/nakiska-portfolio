1231478e7d49c3b87683bf6cc7231205ef391872
XenoverseUp <xenoverseup01@gmail.com>
2023-06-19 09:05:02 +0300

    With easing.

---
+++ config.js
@@ -0,0 +1,3 @@
+// Changes the intro animation speed. The higher ANIMATION_SPEED the faster the intro animation.
+const ANIMATION_SPEED = 6
+export const ANIMATION_SPEED_MULTIPLIER = 5 / ANIMATION_SPEED
--- src/animations/intro.js
+++ src/animations/intro.js
@@ -1,5 +1,6 @@
 import gsap, { Sine, Power3, Power2, Power1, Power4, Expo, Circ } from 'gsap'
 import MotionPathPlugin from 'gsap/MotionPathPlugin'
+import { ANIMATION_SPEED_MULTIPLIER } from '../../config'
 
 gsap.registerPlugin(MotionPathPlugin)
 
@@ -38,18 +39,27 @@ const animateStars = () => {
   gsap
     .timeline()
     .to('#star-0', {
-      delay: 1,
-      duration: 3.5 - 0.5,
+      delay: 1 * ANIMATION_SPEED_MULTIPLIER,
+      duration: 3 * ANIMATION_SPEED_MULTIPLIER,
       motionPath: {
         path: '#star-path-0',
         align: '#star-path-0',
-        autoRotate: true,
         alignOrigin: [0.5, 0.5],
       },
       scale: 0.7,
       immediateRender: true,
       ease: Power4.easeOut,
     })
+    .to(
+      '#star-0',
+      {
+        rotation: `${['+', '-'].sample()}360`,
+        duration: 3.75 * ANIMATION_SPEED_MULTIPLIER,
+        ease: 'none',
+        repeat: -1,
+      },
+      `-=${3 * ANIMATION_SPEED_MULTIPLIER}`
+    )
     .set('#star-0', {
       display: 'none',
     })
@@ -57,12 +67,11 @@ const animateStars = () => {
   gsap
     .timeline()
     .to('#star-1', {
-      delay: 1,
-      duration: 5 - 0.5,
+      delay: 1 * ANIMATION_SPEED_MULTIPLIER,
+      duration: 4.5 * ANIMATION_SPEED_MULTIPLIER,
       motionPath: {
         path: '#star-path-1',
         align: '#star-path-1',
-        autoRotate: true,
         alignOrigin: [0.5, 0.5],
       },
       keyframes: {
@@ -71,6 +80,16 @@ const animateStars = () => {
       immediateRender: true,
       ease: Power4.easeOut,
     })
+    .to(
+      '#star-1',
+      {
+        rotation: `${['+', '-'].sample()}360`,
+        duration: 3.75 * ANIMATION_SPEED_MULTIPLIER,
+        ease: 'none',
+        repeat: -1,
+      },
+      `-=${4.5 * ANIMATION_SPEED_MULTIPLIER}`
+    )
     .set('#star-1', {
       display: 'none',
     })
@@ -78,18 +97,27 @@ const animateStars = () => {
   gsap
     .timeline()
     .to('#star-2', {
-      delay: 1,
-      duration: 5 - 0.5,
+      delay: 1 * ANIMATION_SPEED_MULTIPLIER,
+      duration: 4.5 * ANIMATION_SPEED_MULTIPLIER,
       motionPath: {
         path: '#star-path-2',
         align: '#star-path-2',
-        autoRotate: true,
         alignOrigin: [0.5, 0.5],
       },
       scale: 0.15,
       immediateRender: true,
       ease: Power4.easeOut,
     })
+    .to(
+      '#star-2',
+      {
+        rotation: `${['+', '-'].sample()}360`,
+        duration: 3.75 * ANIMATION_SPEED_MULTIPLIER,
+        ease: 'none',
+        repeat: -1,
+      },
+      `-=${4.5 * ANIMATION_SPEED_MULTIPLIER}`
+    )
     .set('#star-2', {
       display: 'none',
     })
@@ -97,12 +125,11 @@ const animateStars = () => {
   gsap
     .timeline()
     .to('#star-3', {
-      delay: 1,
-      duration: 5.5 - 0.5,
+      delay: 1 * ANIMATION_SPEED_MULTIPLIER,
+      duration: 5 * ANIMATION_SPEED_MULTIPLIER,
       motionPath: {
         path: '#star-path-3',
         align: '#star-path-3',
-        autoRotate: true,
         alignOrigin: [0.5, 0.5],
       },
       keyframes: {
@@ -111,6 +138,16 @@ const animateStars = () => {
       immediateRender: true,
       ease: Power2.easeOut,
     })
+    .to(
+      '#star-3',
+      {
+        rotation: `${['+', '-'].sample()}360`,
+        duration: 3.75 * ANIMATION_SPEED_MULTIPLIER,
+        ease: 'none',
+        repeat: -1,
+      },
+      `-=${5 * ANIMATION_SPEED_MULTIPLIER}`
+    )
     .set('#star-3', {
       display: 'none',
     })
@@ -118,12 +155,11 @@ const animateStars = () => {
   gsap
     .timeline()
     .to('#star-4', {
-      delay: 1,
-      duration: 5 - 0.5,
+      delay: 1 * ANIMATION_SPEED_MULTIPLIER,
+      duration: 4.5 * ANIMATION_SPEED_MULTIPLIER,
       motionPath: {
         path: '#star-path-4',
         align: '#star-path-4',
-        autoRotate: true,
         alignOrigin: [0.5, 0.5],
       },
       keyframes: {
@@ -132,6 +168,16 @@ const animateStars = () => {
       immediateRender: true,
       ease: Power2.easeOut,
     })
+    .to(
+      '#star-4',
+      {
+        rotation: `${['+', '-'].sample()}360`,
+        duration: 3.75 * ANIMATION_SPEED_MULTIPLIER,
+        ease: 'none',
+        repeat: -1,
+      },
+      `-=${4.5 * ANIMATION_SPEED_MULTIPLIER}`
+    )
     .set('#star-4', {
       display: 'none',
     })
@@ -139,12 +185,11 @@ const animateStars = () => {
   gsap
     .timeline()
     .to('#star-5', {
-      delay: 1,
-      duration: 5.5 - 0.5,
+      delay: 1 * ANIMATION_SPEED_MULTIPLIER,
+      duration: 5 * ANIMATION_SPEED_MULTIPLIER,
       motionPath: {
         path: '#star-path-5',
         align: '#star-path-5',
-        autoRotate: true,
         alignOrigin: [0.5, 0.5],
       },
       keyframes: {
@@ -153,6 +198,16 @@ const animateStars = () => {
       immediateRender: true,
       ease: Power2.easeOut,
     })
+    .to(
+      '#star-5',
+      {
+        rotation: `${['+', '-'].sample()}360`,
+        duration: 3.75 * ANIMATION_SPEED_MULTIPLIER,
+        ease: 'none',
+        repeat: -1,
+      },
+      `-=${5 * ANIMATION_SPEED_MULTIPLIER}`
+    )
     .set('#star-5', {
       display: 'none',
     })
@@ -160,18 +215,27 @@ const animateStars = () => {
   gsap
     .timeline()
     .to('#star-6', {
-      delay: 1,
-      duration: 4 - 0.5,
+      delay: 1 * ANIMATION_SPEED_MULTIPLIER,
+      duration: 3.5 * ANIMATION_SPEED_MULTIPLIER,
       motionPath: {
         path: '#star-path-6',
         align: '#star-path-6',
-        autoRotate: true,
         alignOrigin: [0.5, 0.5],
       },
       scale: 0,
       immediateRender: true,
       ease: Power2.easeOut,
     })
+    .to(
+      '#star-6',
+      {
+        rotation: `${['+', '-'].sample()}360`,
+        duration: 3.75 * ANIMATION_SPEED_MULTIPLIER,
+        ease: 'none',
+        repeat: -1,
+      },
+      `-=${3.5 * ANIMATION_SPEED_MULTIPLIER}`
+    )
     .set('#star-6', {
       display: 'none',
     })
@@ -179,18 +243,27 @@ const animateStars = () => {
   gsap
     .timeline()
     .to('#star-7', {
-      delay: 1,
-      duration: 4.5 - 0.5,
+      delay: 1 * ANIMATION_SPEED_MULTIPLIER,
+      duration: 4 * ANIMATION_SPEED_MULTIPLIER,
       motionPath: {
         path: '#star-path-7',
         align: '#star-path-7',
-        autoRotate: true,
         alignOrigin: [0.5, 0.5],
       },
       scale: 0,
       immediateRender: true,
       ease: Power3.easeOut,
     })
+    .to(
+      '#star-7',
+      {
+        rotation: `${['+', '-'].sample()}360`,
+        duration: 3.75 * ANIMATION_SPEED_MULTIPLIER,
+        ease: 'none',
+        repeat: -1,
+      },
+      `-=${4 * ANIMATION_SPEED_MULTIPLIER}`
+    )
     .set('#star-7', {
       display: 'none',
     })
@@ -198,18 +271,27 @@ const animateStars = () => {
   gsap
     .timeline()
     .to('#star-8', {
-      delay: 1,
-      duration: 4.5 - 0.5,
+      delay: 1 * ANIMATION_SPEED_MULTIPLIER,
+      duration: 4 * ANIMATION_SPEED_MULTIPLIER,
       motionPath: {
         path: '#star-path-8',
         align: '#star-path-8',
-        autoRotate: true,
         alignOrigin: [0.5, 0.5],
       },
       scale: 0,
       immediateRender: true,
       ease: Power3.easeOut,
     })
+    .to(
+      '#star-8',
+      {
+        rotation: `${['+', '-'].sample()}360`,
+        duration: 3.75 * ANIMATION_SPEED_MULTIPLIER,
+        ease: 'none',
+        repeat: -1,
+      },
+      `-=${4 * ANIMATION_SPEED_MULTIPLIER}`
+    )
     .set('#star-8', {
       display: 'none',
     })
@@ -217,18 +299,27 @@ const animateStars = () => {
   gsap
     .timeline()
     .to('#star-9', {
-      delay: 1,
-      duration: 4.5 - 0.5,
+      delay: 1 * ANIMATION_SPEED_MULTIPLIER,
+      duration: 4 * ANIMATION_SPEED_MULTIPLIER,
       motionPath: {
         path: '#star-path-9',
         align: '#star-path-9',
-        autoRotate: true,
         alignOrigin: [0.5, 0.5],
       },
       scale: 0,
       immediateRender: true,
       ease: Power3.easeOut,
     })
+    .to(
+      '#star-9',
+      {
+        rotation: `${['+', '-'].sample()}360`,
+        duration: 3.75 * ANIMATION_SPEED_MULTIPLIER,
+        ease: 'none',
+        repeat: -1,
+      },
+      `-=${4 * ANIMATION_SPEED_MULTIPLIER}`
+    )
     .set('#star-9', {
       display: 'none',
     })
@@ -238,12 +329,11 @@ const animateBubbles = () => {
   gsap
     .timeline()
     .to('#bubble-0', {
-      duration: 5.5,
-      delay: 1.6,
+      duration: 5.5 * ANIMATION_SPEED_MULTIPLIER,
+      delay: 1.6 * ANIMATION_SPEED_MULTIPLIER,
       motionPath: {
         path: '#bubble-path-0',
         align: '#bubble-path-0',
-        autoRotate: true,
         alignOrigin: [0.5, 0.5],
       },
       scale: 0.2,
@@ -257,12 +347,11 @@ const animateBubbles = () => {
   gsap
     .timeline()
     .to('#bubble-1', {
-      duration: 5,
-      delay: 1.5,
+      duration: 5 * ANIMATION_SPEED_MULTIPLIER,
+      delay: 1.5 * ANIMATION_SPEED_MULTIPLIER,
       motionPath: {
         path: '#bubble-path-1',
         align: '#bubble-path-1',
-        autoRotate: true,
         alignOrigin: [0.5, 0.5],
       },
       scale: 0.1,
@@ -276,12 +365,11 @@ const animateBubbles = () => {
   gsap
     .timeline()
     .to('#bubble-2', {
-      duration: 6,
-      delay: 1.3,
+      duration: 6 * ANIMATION_SPEED_MULTIPLIER,
+      delay: 1.3 * ANIMATION_SPEED_MULTIPLIER,
       motionPath: {
         path: '#bubble-path-2',
         align: '#bubble-path-2',
-        autoRotate: true,
         alignOrigin: [0.5, 0.5],
       },
       scale: 0.2,
@@ -295,12 +383,11 @@ const animateBubbles = () => {
   gsap
     .timeline()
     .to('#bubble-3', {
-      duration: 6,
-      delay: 1.35,
+      duration: 6 * ANIMATION_SPEED_MULTIPLIER,
+      delay: 1.35 * ANIMATION_SPEED_MULTIPLIER,
       motionPath: {
         path: '#bubble-path-3',
         align: '#bubble-path-3',
-        autoRotate: true,
         alignOrigin: [0.5, 0.5],
       },
       scale: 0.2,
