6c1d6e9a8b88a6d3186be233ecc4c71a487bbe79
XenoverseUp <xenoverseup@gmail.com>
2023-07-05 14:56:48 +0300

    Media player functionality.

--- src/components/ReelPlayer.jsx
+++ src/components/ReelPlayer.jsx
@@ -6,6 +6,7 @@ import {
   useMediaPlaying,
 } from '@react-av/core'
 import { useLayoutEffect } from 'react'
+import styles from '../styles/components/ReelPlayer.module.scss'
 
 function PlayPauseButton() {
   const [playing, setPlaying] = useMediaPlaying()
@@ -13,8 +14,38 @@ function PlayPauseButton() {
   useLayoutEffect(() => setPlaying(true), [])
 
   return (
-    <button data-cursor-hover onClick={() => setPlaying(!playing)}>
-      {playing ? 'Pause' : 'Play'}
+    <button
+      className={styles.playButton}
+      data-cursor-hover
+      onClick={() => setPlaying(!playing)}
+    >
+      {!playing ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            fillRule="evenodd"
+            d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            fillRule="evenodd"
+            d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z"
+            clipRule="evenodd"
+          />
+        </svg>
+      )}
     </button>
   )
 }
@@ -22,10 +53,13 @@ function PlayPauseButton() {
 const ReelPlayer = () => {
   return (
     <Root>
-      <Container>
-        <Video src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" />
+      <Container className={styles.container}>
+        <Video
+          className={styles.video}
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        />
       </Container>
-      <Viewport>
+      <Viewport inactiveClassName={styles.inactive} className={styles.viewport}>
         <PlayPauseButton />
       </Viewport>
     </Root>
